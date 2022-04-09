d17b9ea5789714cb48a29dee85aa3d3d66adab7a
Julia Catalano <catalano.43@osu.edu>
2022-04-08 20:46:29 -0400

    define osu colors and change toggle bg to scarlet

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,6 @@ import {DarkToggle} from "./components/themes/DarkToggle";
 export default function App() {
     return (
       <>
-        <div className="TextCard">
-          <h2 className="TextCard__Title">Temperature for plants 🌱</h2>
-          <p className="TextCard__Body">
-            The proper condition of temperature is the most difficult thing to
-            regulate and maintain in growing plants in the house.
-          </p>
-          <button className="Button">Learn more</button>
-        </div>
   
         <DarkToggle />
       </>

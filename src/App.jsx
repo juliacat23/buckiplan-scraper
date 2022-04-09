edb795b16dbece71d4b95dc2a63f10e521addbfe
Julia Catalano <catalano.43@osu.edu>
2022-04-09 10:32:21 -0400

    config react router for multipage app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 import "./assets/sass/_index.scss";
 import "react-toggle/style.css";
 
-import {DarkToggle} from "./components/themes/DarkToggle";
+// import {DarkToggle} from "./components/themes/DarkToggle";
+import Login from './views/Login';
 
 export default function App() {
     return (
-      <>
-        <DarkToggle />
-      </>
+      <Router>
+        <Routes>
+          <Route exact path='/login' exact element={<Login />} />
+        </Routes>
+      </Router>
     );
   }
---
+++ src/views/Login.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Login() {
+    return (
+        <div class="landing top-sec">
+            <div class="container p-0 m-0">
+                <div class="col">
+                    <div class="row no-gutters top-section">
+                        <h1 class="plan-head mt-5">
+                            <div>Schedule Smarter</div>
+                            <div>Plan Ahead</div>
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login;

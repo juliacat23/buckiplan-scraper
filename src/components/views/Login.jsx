5c5a022944fb2cdf5c12f968b6b925ceecd1b4de
Julia Catalano <catalano.43@osu.edu>
2022-04-09 17:22:28 -0400

    test UI design ideas (no function)

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -9,12 +9,27 @@ class Login extends React.Component {
                         <div className='row top-bar phonepad no-gutters'>
                             <div className='col'>
                                 <div className='row no-gutters top-section'>
-                                    <div>Schedule Smarter</div>
-                                    <div>Plan Ahead</div>
+                                    <h1 className='plan-head mt-5'>
+                                        <div>Schedule Smarter</div>
+                                        <div>Plan Ahead</div>
+                                    </h1>
+                                </div>
+                                <div className='row no-gutters top-section'>
+                                    <div className='plan-subhead'>
+                                        <div>Introducting the newest and easiest</div>
+                                        <div>way to plan courses at OSU</div>
+                                    </div>
+                                </div>
+                                <div className='row justify-content-center'>
+                                    <div className='col top-section'>
+                                        <button className='email-button email-button--top' variant='primary'>
+                                            Get Started
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>            
+                    </div>
                 </div>
             </React.Fragment>
         )

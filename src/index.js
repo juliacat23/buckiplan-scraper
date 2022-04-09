5cb2929e5c738b9b508d6e8984ea3304a0c6d985
Julia Catalano <catalano.43@osu.edu>
2022-04-09 17:33:33 -0400

    pass new apollo client instance

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+import {
+    ApolloProvider,
+    ApolloClient,
+    createHttpLink,
+    InMemoryCache,
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000', /*  graphql server will be running on localhost:4000 */
+});
+
+/* instantiate ApolloClient by passing 
+in the httpLink and a new instance
+of in InMemoryCache() */
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+);

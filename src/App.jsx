e7aefc8f8c23d87df46c609ba517b9333ac8d429
Julia Catalano <catalano.43@osu.edu>
2022-04-11 08:43:40 -0400

    redirect authenticated users to dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import "./assets/sass/App.scss";
-import "react-toggle/style.css";
 
 // import {DarkToggle} from "./components/themes/DarkToggle";
-import Login from './components/views/Login';
 
 export default function App() {
     return (
--- src/index.js
+++ src/index.js
@@ -2,28 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import {
-    ApolloProvider,
-    ApolloClient,
-    createHttpLink,
-    InMemoryCache,
-} from '@apollo/client';
-
-const httpLink = createHttpLink({
-    uri: 'http://localhost:4000', /*  graphql server will be running on localhost:4000 */
-});
-
-/* instantiate ApolloClient by passing 
-in the httpLink and a new instance
-of in InMemoryCache() */
-const client = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache(),
-});
-
 ReactDOM.render(
-    <ApolloProvider client={client}>
+    <React.StrictMode>
         <App />
-    </ApolloProvider>,
+    </React.StrictMode>,
     document.getElementById('root')
 );

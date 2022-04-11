18fb9d06c14d585462cb21dcd7de4018c0016943
Julia Catalano <catalano.43@osu.edu>
2022-04-11 08:33:47 -0400

    use context API to track authentication status of user

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import {signInWithGoogle} from '../services/firebase';
 
-const Login = () => {
-    return (
-      <div>
-        <button className="email-button email-button--top" variant="primary" onClick={signInWithGoogle}><i className="fab fa-google"></i>Sign in with google</button>
-      </div>
-    )
-  }
-  
-  export default Login;
+export default function Login() {
+  return (
+    <div className="login-buttons">
+      
+    </div>
+  )
+}

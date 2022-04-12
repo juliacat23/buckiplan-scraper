ed7b549532e28c54574f613293e2fea0eda932a7
Julia Catalano <catalano.43@osu.edu>
2022-04-12 10:35:26 -0400

    seperate navbar into individualizied components

---
+++ src/components/AuthNav.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AuthButton from './AuthButton';
+
+const AuthNav = () => (
+    <div className='navbar-nav ml-auto'>
+        <AuthButton />
+    </div>
+);
+
+export default AuthNav;

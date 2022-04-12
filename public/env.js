f21d72187b91b319411dc9eedfbcaf974eae5e0b
Julia Catalano <catalano.43@osu.edu>
2022-04-12 18:31:31 -0400

    delete

--- public/env.js
+++
@@ -1,8 +0,0 @@
-window.env = {
-  "APIKEY": "AIzaSyBcm3lYk_Kgb18hkSzqKvv7Yn_SKOhqk5E",
-  "AUTHDOMAIN": "buckiplan-dev.firebaseapp.com",
-  "DATABASEURL": "https://buckiplan-dev-default-rtdb.firebaseio.com",
-  "PROJECTID": "buckiplan-dev",
-  "MESSAGINGSENDERID": "225554951411",
-  "APPID": "1:225554951411:web:ad15e9537375ed4c5a5b86"
-};
--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import HomeContent from './HomeContent';
-import Hero from './Hero';
-import Loading from './Loading';
-import NavBar from './NavBar';
-
-export { HomeContent, Hero, Loading, NavBar };

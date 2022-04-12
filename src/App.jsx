9637a902ff4af243e69bcdba3237d367f0a83c23
Julia Catalano <catalano.43@osu.edu>
2022-04-12 10:22:19 -0400

    init auth0 authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,9 @@
-import React, { Component } from 'react';
-import withFirebaseAuth from 'react-with-firebase-auth'
-import firebase from 'firebase/compat/app'; // fix to import error
-import 'firebase/auth';
-import firebaseConfig from './config/firebaseConfig';
-import logo from './logo.svg';
-import './App.css';
-
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-class App extends Component {
-  render() {
-    const {
-      user,
-      signOut,
-      signInWithGoogle,
-    } = this.props;
-
+function App() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          {
-            user
-              ? <p>Hello, {user.displayName}</p>
-              : <p>Please sign in.</p>
-          }
-
-          {
-            user
-              ? <button onClick={signOut}>Sign out</button>
-              : <button onClick={signInWithGoogle}>Sign in with Google</button>
-          }
-        </header>
-      </div>
+        <div className='App'>
+            <h1>Learn React</h1>
+        </div>
     );
-  }
 }
 
-const firebaseAppAuth = firebaseApp.auth();
-
-const providers = {
-  googleProvider: new firebase.auth.GoogleAuthProvider(),
-};
-
-export default withFirebaseAuth({
-  providers,
-  firebaseAppAuth,
-})(App);
+export default App;

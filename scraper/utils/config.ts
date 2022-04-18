c5611feff642fd4bebb201d5ab17e23a8027393d
Julia Catalano <catalano.43@osu.edu>
2022-04-18 10:11:17 -0400

    add function to create default config file if one does not exist

--- scraper/utils/config.ts
+++ scraper/utils/config.ts
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import {is} from 'typescript-is';
+import { is } from 'typescript-is';
 import { promisify } from 'util';
 
 const readFile = promisify(fs.readFile);
@@ -8,40 +8,45 @@ const mkdir = promisify(fs.mkdir);
 
 export async function readConfigFromDisk(): Promise<ConfigRoot> {
     console.log('readConfigFromDisk');
-    const configLoc = '/config/config.json'
+    const configLoc = '/config/config.json';
 
     if (fs.existsSync('/config')) {
         // if file exists
         if (fs.existsSync(configLoc)) {
             // read it
-            let configRaw: any = await readFile(configLoc, { encoding: 'utf-8' });
+            let configRaw: any = await readFile(configLoc, {
+                encoding: 'utf-8',
+            });
             // check its type
-            let configData = null
+            let configData = null;
             try {
                 configData = JSON.parse(configRaw);
-            }
-            catch(err) { }
+            } catch (err) {}
 
             // automatically check type guard
-            if(is<ConfigRoot>(configData)) {
+            if (is<ConfigRoot>(configData)) {
                 // if type is good
                 return configData;
             }
         }
-    }
-    else {
-        await mkdir('/config')
+    } else {
+        await mkdir('/config');
     }
 
     // if a valid file wasn't found, create a new one
     let sampleConfig: ConfigRoot = {
         Courses: [
-            {Subject: 'BIO', CatalogNumber: '1113', SemesterCode: '1222', DesiredSectionNumbers: []}
-        ], 
+            {
+                Subject: 'BIO',
+                CatalogNumber: '1113',
+                SemesterCode: '1222',
+                DesiredSectionNumbers: [],
+            },
+        ],
         Telegram: {
             BotToken: 'jladsoiasdmnaisdnasdh',
-            ChatId: '-4467267424'
-        }
+            ChatId: '-4467267424',
+        },
     };
 
     await writeFile(configLoc, JSON.stringify(sampleConfig, null, 1), 'utf-8');
@@ -49,20 +54,20 @@ export async function readConfigFromDisk(): Promise<ConfigRoot> {
 }
 
 export interface ConfigRoot {
-    Courses: ConfigCourse[],
-    Telegram: ConfigTelegram
+    Courses: ConfigCourse[];
+    Telegram: ConfigTelegram;
 }
 
 export interface ConfigCourse {
-    Subject: string,
-    CatalogNumber: string, 
-    SemesterCode: string,
-    DesiredSectionNumbers: string[]
+    Subject: string;
+    CatalogNumber: string;
+    SemesterCode: string;
+    DesiredSectionNumbers: string[];
 }
 
 export interface ConfigTelegram {
-    BotToken: string,
-    ChatId: string
+    BotToken: string;
+    ChatId: string;
 }
 
-export const SAMPLE_PSID = '123456'
+export const SAMPLE_PSID = '123456';

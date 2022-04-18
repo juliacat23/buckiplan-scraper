e7b0bd5092d730d56aa62d4c32deec41d328038a
Julia Catalano <catalano.43@osu.edu>
2022-04-18 10:10:24 -0400

    pretty print telegram status

--- scraper/index.ts
+++ scraper/index.ts
@@ -18,65 +18,75 @@ import * as prettyPrint from './utils/prettyPrint';
 
 (async () => {
     let config = await readConfigFromDisk();
-  })();
-  
-  const jobs = new Queue('Jobs');
-  setQueues([ new BullAdapter(jobs) ]);
-  
-  jobs.process(async (job) => {
-    console.log('Job spawned!')
-  
+})();
+
+const jobs = new Queue('Jobs');
+setQueues([new BullAdapter(jobs)]);
+
+jobs.process(async (job) => {
+    console.log('Job spawned!');
+
     // Read config from disk (will also create if it doesn't exist)
     const config = await readConfigFromDisk();
-  
+
     // Business logic
     let i = 0;
-  
+
     // for every course that we want to scrape
     for (let course of config.Courses) {
-      // scrape its sections
-      let sections = await puppet.scrape(course);
-      // update progress
-      await job.progress(Math.ceil((i++ / config.Courses.length) * 100));
-      // for every section in the results
-      for (let section of sections) {
-        // check if the section is "open" and if its a section we even care about
-        let willSendNotification = false;
-        // if no sections are provided, send a notification for ANY open section
-        if (course.DesiredSectionNumbers.length === 0) {
-          if (section.registrationStatus !== 'Closed' && section.registrationStatus !== 'Wait List') {
-            willSendNotification = true;
-          }
-        }
-        else {
-          if (section.registrationStatus !== 'Closed' && section.registrationStatus !== 'Wait List' && course.DesiredSectionNumbers.includes(section.sectionIdentifier)) {
-            willSendNotification = true;
-          }
-        }
-  
-        if (willSendNotification) {
-          prettyPrint.cyan('📧 Telegram message sent!');
-          await telegram.sendMessage(config.Telegram, `
+        // scrape its sections
+        let sections = await puppet.scrape(course);
+        // update progress
+        await job.progress(Math.ceil((i++ / config.Courses.length) * 100));
+        // for every section in the results
+        for (let section of sections) {
+            // check if the section is "open" and if its a section we even care about
+            let willSendNotification = false;
+            // if no sections are provided, send a notification for ANY open section
+            if (course.DesiredSectionNumbers.length === 0) {
+                if (
+                    section.registrationStatus !== 'Closed' &&
+                    section.registrationStatus !== 'Wait List'
+                ) {
+                    willSendNotification = true;
+                }
+            } else {
+                if (
+                    section.registrationStatus !== 'Closed' &&
+                    section.registrationStatus !== 'Wait List' &&
+                    course.DesiredSectionNumbers.includes(
+                        section.sectionIdentifier
+                    )
+                ) {
+                    willSendNotification = true;
+                }
+            }
+
+            if (willSendNotification) {
+                prettyPrint.cyan('📧 Telegram message sent!');
+                await telegram.sendMessage(
+                    config.Telegram,
+                    `
   <b><u>${course.Subject} ${course.CatalogNumber} is ${section.registrationStatus}!</u></b> 
   Section #: <code>${section.sectionIdentifier}</code>
   Registration Status: <code>${section.registrationStatus}</code>
   Instructor: <code>${section.instructor}</code>
   Meeting Time: <code>${section.meetingTimeWritten}</code>
   Instruction Mode: <code>${section.instructionMode}</code>
-          `);
+          `
+                );
+            }
         }
-  
-      }
     }
-  
+
     return;
-  });
-  
-  jobs.add({}, { repeat: { cron: '*/5 * * * *' } })
-  //jobs.add({})
-  
-  const app = express();
-  app.use('/', router);
-  app.listen(1234, () => {
+});
+
+jobs.add({}, { repeat: { cron: '*/5 * * * *' } });
+//jobs.add({})
+
+const app = express();
+app.use('/', router);
+app.listen(1234, () => {
     console.log(`Example app listening at http://localhost:1234`);
-  });
+});

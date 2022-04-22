0d66c4affcb4538c32661938e780b6b2c9943620
Julia Catalano <catalano.43@osu.edu>
2022-04-22 15:10:27 -0400

    configure api auth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,28 @@
+import NextAuth from 'next-auth/next';
+import EmailProvider from 'next-auth/providers/email';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import FacebookProvider from 'next-auth/providers/facebook';
+import Facebook from 'next-auth/providers/facebook';
+
+export default NextAuth({
+    // configure one or more authentication providers
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: proccess.env.GITHUB_SECRET,
+        }),
+        EmailProvider({
+            server: process.env.EMAIL_SERVER,
+            from: process.env.EMAIL_FROM,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+        }),
+        FacebookProvider({
+            clientId: process.env.FACEBOOK_ID,
+            clientSecret: process.env.FACEBOOK_SECRET,
+        }),
+    ],
+});

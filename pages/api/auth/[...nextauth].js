8e657cd6eecc527af255b1ef9972149997a11794
Julia Catalano <catalano.43@osu.edu>
2022-04-22 15:25:55 -0400

    configure nextauth to use the prisma adapter

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,18 @@
 import NextAuth from 'next-auth/next';
+
 import EmailProvider from 'next-auth/providers/email';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import FacebookProvider from 'next-auth/providers/facebook';
-import Facebook from 'next-auth/providers/facebook';
+
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export default NextAuth({
     // configure one or more authentication providers
+    adapter: PrismaAdapter(prisma),
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID,

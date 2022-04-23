9867f70daa0632def4f1276bb9a36327aa1611f1
Julia Catalano <catalano.43@osu.edu>
2022-04-23 15:25:14 -0400

    resolve import error and define header layout

---
+++ components/Header.js
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+
+import '../styles/Header.module.scss';
+
+const Header = (props) => (
+    <div className='landing top-sec'>
+        <div className='container p-0 m-0'>
+            <div className='row top-bar phonepad no-gutters'>
+                <div className='col'>
+                    <div className='row no-gutters top-section'>
+                        <h1 className='plan-head mt-5'>
+                            <div>Schedule Smarter</div>
+                            <div>Plan Ahead</div>
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default Header;

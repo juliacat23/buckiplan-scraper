31142ce8caf761afcc0c34cee091843741787349
Julia Catalano <catalano.43@osu.edu>
2022-04-23 16:04:24 -0400

    expose postcss to build process

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,20 @@
 import Head from 'next/head';
 
-import '../styles/Header.module.scss';
+import styles from '../styles/Header.module.scss';
 
 const Header = () => (
-    <div className='landing top-sec'>
-        <div className='container p-0 m-0'>
-            <div className='row top-bar phonepad no-gutters'>
-                <div className='col'>
+    <div className={styles.topSec}>
+        <div className={styles.container}>
+            <div className={styles.topBar}>
+                <div className={styles.Col}>
                     <div className='row no-gutters top-section'>
-                        <h1 className='plan-head mt-5'>
+                        <h1 className={styles.planHead}>
                             <div>Schedule Smarter</div>
                             <div>Plan Ahead</div>
                         </h1>
+                    </div>
+                    <div className='row no-gutters top-section'>
+                        <div className='plan-subhead'></div>
                     </div>
                 </div>
             </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,22 @@
-import '../styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
 
+import Head from 'next/head';
+import 'bootstrap/dist/css/bootstrap.css';
 // expose session context at top level
 export default function App({
     Component,
     pageProps: { session, ...pageProps },
 }) {
     return (
-        <SessionProvider session={session}>
+        <>
+            <Head>
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1'
+                />
+            </Head>
+
             <Component {...pageProps} />
-        </SessionProvider>
+        </>
     );
 }

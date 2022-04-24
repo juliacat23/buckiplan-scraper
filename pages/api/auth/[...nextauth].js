73e36d7180987b41936601405874335520e0a416
Julia Catalano <catalano.43@osu.edu>
2022-04-24 16:25:33 -0400

    add sign up route

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,41 +1,56 @@
 import NextAuth from 'next-auth/next';
 
-import EmailProvider from 'next-auth/providers/email';
-import GithubProvider from 'next-auth/providers/github';
-import GoogleProvider from 'next-auth/providers/google';
-// import FacebookProvider from 'next-auth/providers/facebook';
-
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { compare } from 'bcryptjs';
+import { CredentialsProvider } from 'next-auth/providers';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export default NextAuth({
-    // configure one or more authentication providers
-    adapter: PrismaAdapter(prisma),
+    session: {
+        strategy: 'jwt',
+        maxAge: 60 * 60, // 1 hour
+    },
     providers: [
-        GithubProvider({
-            clientId: process.env.NEXT_PUBLIC_GITHUB_ID,
-            clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET,
-        }),
-        EmailProvider({
-            server: {
-                host: process.env.NEXT_PUBLIC_EMAIL_SERVER_HOST,
-                port: process.env.EMAIL_SERVER_PORT,
-                auth: {
-                    user: process.env.NEXT_PUBLIC_EMAIL_SERVER_USER,
-                    pass: process.env.NEXT_PUBLIC_EMAIL_SERVER_PASSWORD,
-                },
+        CredentialsProvider({
+            name: 'credentials',
+            async authorize(credentials) {
+                // check if user is in db
+                const user = await prisma.user.findUnique({
+                    where: {
+                        user_name: credentials.user_name,
+                    },
+                });
+                if (!user) {
+                    return null;
+                }
+
+                // check if password is correct
+                const valid = await compare(
+                    credentials.password,
+                    user.password
+                );
+                if (!valid) {
+                    return null;
+                }
+                return user;
             },
-            from: process.env.NEXT_PUBLIC_EMAIL_FROM,
         }),
-        GoogleProvider({
-            clientId: process.env.NEXT_PUBLIC_GOOGLE_ID,
-            clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET,
-        }),
-        // FacebookProvider({
-        //     clientId: process.env.FACEBOOK_ID,
-        //     clientSecret: process.env.FACEBOOK_SECRET,
-        // }),
     ],
+    callbacks: {
+        async jwt({ token, account, user }) {
+            if (user) {
+                token.id = user.id;
+                token.user_name = token.user_name;
+            }
+            return token;
+        },
+        async session({ session, token, user }) {
+            if (token) {
+                session.user.name = token.user_name;
+            }
+            return session;
+        },
+    },
+    secret: process.env.JWT_SECRET,
 });

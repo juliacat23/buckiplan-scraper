6c7c105e73d1feb2ed4f55cf7d6b40c75f43b1cb
Julia Catalano <catalano.43@osu.edu>
2022-04-24 18:04:54 -0400

    implement various actions

---
+++ actions/boardActions.js
@@ -0,0 +1,35 @@
+import { actionTypes } from '../constants/actionTypes';
+
+export const setBoard = (newBoard) => {
+    return {
+        type: actionTypes.SET_BOARD,
+        payload: {
+            newBoard,
+        },
+    };
+};
+
+export const addExtraYear = () => {
+    return {
+        type: actionTypes.ADD_EXTRA_YEAR,
+        payload: {},
+    };
+};
+
+export const deleteExtraYear = (yearIndex) => {
+    return {
+        type: actionTypes.DELETE_EXTRA_YEAR,
+        payload: {
+            yearIndex,
+        },
+    };
+};
+
+export const addPDFCourses = (courses) => {
+    return {
+        type: actionTypes.ADD_PDF_COURSES,
+        payload: {
+            courses,
+        },
+    };
+};
---
+++ actions/courseActions.js
@@ -0,0 +1,50 @@
+import { actionTypes } from '../constants/actionTypes';
+
+export const addCourse = (courseName, yearIndex, semesterIndex) => {
+    return {
+        type: actionTypes.ADD_COURSE,
+        payload: {
+            courseName,
+            yearIndex,
+            semesterIndex,
+        },
+    };
+};
+
+export const deleteCourse = (yearIndex, semesterIndex, courseIndex) => {
+    return {
+        type: actionTypes.DELETE_COURSE,
+        payload: {
+            yearIndex,
+            semesterIndex,
+            courseIndex,
+        },
+    };
+};
+
+export const editCourse = (yearIndex, semesterIndex, courseIndex) => {
+    return {
+        type: actionTypes.EDIT_COURSE,
+        payload: {
+            yearIndex,
+            semesterIndex,
+            courseIndex,
+        },
+    };
+};
+
+export const loadJsonCourses = (json) => {
+    return {
+        type: actionTypes.LOAD_COURSE,
+        payload: {
+            json,
+        },
+    };
+};
+
+export const exportCourses = () => {
+    return {
+        type: actionTypes.EXPORT_COURSES,
+        payload: {},
+    };
+};

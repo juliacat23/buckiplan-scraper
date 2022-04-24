974f9618acdc6573e98830e26fb42aa9e55a0f45
Julia Catalano <catalano.43@osu.edu>
2022-04-24 12:28:27 -0400

    configure shared session state

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
 import '../styles/header.css';
+
+import { SessionProvider } from 'next-auth/react';
 // import '../styles/hero.css';
 
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+export default function App({
+    Component,
+    pageProps: { session, ...pageProps },
+}) {
+    return (
+        <SessionProvider session={session}>
+            <Component {...pageProps} />
+        </SessionProvider>
+    );
 }
-export default MyApp;

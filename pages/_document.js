509ad34a8af1f047e3e2045c47b86d018f740795
Julia Catalano <catalano.43@osu.edu>
2022-04-25 16:46:39 -0400

    define themeing

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,30 @@
-// pages/_document.js
+import Document from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-import { ColorModeScript } from '@chakra-ui/react';
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
-import theme from '../theme/theme';
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
 
-export default class Document extends NextDocument {
-    render() {
-        return (
-            <Html lang="en">
-                <Head />
-                <body>
-                    {/*  color mode script needs to be added before the content inside the <body /> tag */}
-                    <ColorModeScript
-                        initialColorMode={theme.config.initialColorMode}
-                    />
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) =>
+                        sheet.collectStyles(<App {...props} />),
+                });
+
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            };
+        } finally {
+            sheet.seal();
+        }
     }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,45 @@
-const Index = () => {
-    return <div> Hello World!</div>;
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { useColorMode } from '@chakra-ui/color-mode';
+import React from 'react';
+import { Heading } from '@chakra-ui/layout';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { IconButton } from '@chakra-ui/button';
+
+const Home = () => {
+    const { colorMode, toggleColorMode } = useColorMode();
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Dark mode using Next.js and Chakra UI</title>
+                <meta
+                    name="description"
+                    content="Dark mode using Next.js and Chakra UI"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main className={styles.main}>
+                <Heading>
+                    {' '}
+                    Dark Mode toggle using Chakra UI and Next.js{' '}
+                </Heading>
+
+                <IconButton
+                    mt={4}
+                    aria-label="Toggle Mode"
+                    onClick={toggleColorMode}
+                >
+                    {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+                </IconButton>
+            </main>
+
+            <footer className={styles.footer}>
+                Built in ♥️ with Next.js and Chakra UI
+            </footer>
+        </div>
+    );
 };
-export default Index;
+
+export default Home;

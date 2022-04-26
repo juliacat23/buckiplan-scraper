3fef383c9b2b5c695ddaaa1333b6e445dd90b05d
Julia Catalano <catalano.43@osu.edu>
2022-04-26 09:53:05 -0400

    define user endpoint

--- pages/api/auth/[...auth0].js
+++
@@ -1,3 +0,0 @@
-import { handleAuth, handleLogin, handleLogout } from '@auth0/nextjs-auth0';
-
-export default handleAuth();
---
+++ pages/api/users/authenticate.js
@@ -0,0 +1,3 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import getConfig from 'next/config';

a34d6435de21f044c10d864399a632e481f85421
Julia Catalano <catalano.43@osu.edu>
2022-05-02 09:49:08 -0400

    configure linter and formatter

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
+    presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-typescript',
+    ],
+    plugins: [
+        () => ({
+            visitor: {
+                MetaProperty(path) {
+                    path.replaceWithSourceString('process');
+                },
+            },
+        }),
+    ],
 };

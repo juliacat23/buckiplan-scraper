6ce17f5fc21751675ea67fdba0395b2e558170da
Julia Catalano <catalano.43@osu.edu>
2022-05-03 09:53:34 -0400

    fetch courses at api/course/[subject]/[catalog_number]

--- pages/api/course/[subject]/[catalog_number]/index.js
+++ pages/api/course/[subject]/[catalog_number]/index.js
@@ -1,10 +1,11 @@
-import prisma from '../../../../lib/prisma';
+import prisma from '../../../../../lib/prisma';
 
 export default async function handle(req, res) {
     const subject = req.query.subject;
+    const catalog_number = req.query.catalog_number;
 
     if (req.method === 'GET') {
-        handleGET(subject, res);
+        handleGET(subject, catalog_number, res);
     } else {
         throw new Error(
             `The HTTP ${req.method} method is not supported at this route.`
@@ -14,9 +15,12 @@ export default async function handle(req, res) {
 
 // GET /api/course/:id
 
-async function handleGET(subject, res) {
+async function handleGET(subject, catalog_number, res) {
     const course = await prisma.course.findMany({
-        where: { subject: String(subject) },
+        where: {
+            subject: String(subject),
+            catalog_number: String(catalog_number),
+        },
     });
     res.json(course);
 }

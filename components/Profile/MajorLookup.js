b457f7e2750d9b200783f8ee4a4d85d503aeb49d
Julia Catalano <catalano.43@osu.edu>
2022-05-04 13:26:52 -0400

    dynamic dropdown

---
+++ components/Profile/MajorLookup.js
@@ -0,0 +1,12 @@
+import React, { useMemo, useState } from 'react';
+
+const MajorLookup = {
+    'Arts and Sciences': 
+   
+}
+
+export const ARC = [
+    'Architecture',
+    'City and Regional Planning',
+    'Landscape Architecture',
+];
--- pages/profile.js
+++ pages/profile.js
@@ -15,71 +15,11 @@ import {
     Select,
 } from '@chakra-ui/react';
 
+import { useState } from 'react';
+
 import { SmallCloseIcon } from '@chakra-ui/icons';
 
-import { useSelect } from 'react';
-
 export default function Profile() {
-    const [selected, setSelected] = useState('');
-    const changeSelectOptionHandler = (event) => {
-        setSelected(event.target.value);
-    };
-
-    const ARC = [
-        'Architecture',
-        'City and Regional Planning',
-        'Landscape Architecture',
-    ];
-    const BUS = [
-        'Accounting',
-        'Aviation Management',
-        'Business Economics',
-        'Finance',
-        'Human Resources',
-        'Information Systems',
-        'Insurance',
-        'International Business',
-        'Logistics Management',
-        'Marketing',
-        'Operations Management',
-        'Real Estate and Urban Analysis',
-    ];
-    const EHE = [
-        'Child and Youth Studies',
-        'Consumer and Family Financial Services',
-        'Education (Early Childhood)',
-        'Education (Integrated Languate Arts/English)',
-        'Education (Middle Childhood)',
-        'Education (Science and Mathematics)',
-        'Education (Special Ed)',
-        'Education (Teaching English to Speakers of Other Languages)',
-        'Education (Technical Education and Training)',
-        'Education (World Language)',
-        'Exercise Science Education',
-        'Fashion and Retail Studies',
-        'Human Promotion, Nutrition and Exercise Science',
-        'Hospitality Management',
-        'Human Development and Family Science',
-        'Human Nutrition',
-        'Sport Industry',
-        'Sports Coaching, Recreation and Physical Education',
-    ];
-    const ENG = [
-        'Aerospace Engineering',
-        'Aviation',
-        'Biomedical Engineering',
-        'Chemical Engineering',
-        'Civil Engineering',
-        'Computer Science and Engineering',
-        'Electrical and Computer Engineering',
-        'Engineering Physics',
-        'Environmental Engineering',
-        'Food, Agricultural and Biological Engineering',
-        'Industrial and Systems Engineering',
-        'Material Science and Engineering',
-        'Mechnical Engineering',
-        'Welding Engineering',
-    ];
     return (
         <Flex
             minH={'100vh'}
@@ -123,33 +63,6 @@ export default function Profile() {
                 </FormControl>
                 <FormControl id="college" isRequired>
                     <FormLabel>Your College</FormLabel>
-                    <Select
-                        placeholder="Select One"
-                        _placeholder={{ color: 'gray.500' }}
-                    >
-                        <option value="ASC">Arts and Sciences</option>
-                        <option value="DEN">Denistry</option>
-                        <option value="DEN">Education and Human Ecology</option>
-                        <option value="ENG">Engineering</option>
-                        <option value="BUS">Fisher College of Business</option>
-                        <option value="FAE">
-                            Food, Agricultural, and Environmental Sciences
-                        </option>
-                        <option value="HRS">
-                            Health and Rehabilitation Sciences
-                        </option>
-                        <option value="JGL">
-                            John Glenn College of Public Affairs
-                        </option>
-                        <option value="ARC">
-                            Knowlton School of Architecture
-                        </option>
-                        <option value="MED">Medicine</option>
-                        <option value="NUR">Nursing</option>
-                        <option value="PHR">Pharmacy</option>
-                        <option value="PUB">Public Health</option>
-                        <option value="SWK">Social Work</option>
-                    </Select>
                 </FormControl>
                 <FormControl id="email" isRequired>
                     <FormLabel>Email address</FormLabel>

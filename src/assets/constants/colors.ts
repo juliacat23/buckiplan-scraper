fa1d6c63565cd96916aab07825ba11a7f296ba1a
Julia Catalano <catalano.43@osu.edu>
2022-05-09 10:32:26 -0400

    define types for course colors

---
+++ src/assets/constants/colors.ts
@@ -0,0 +1,56 @@
+export const reviewColors = {
+    red: '#d9534f',
+    orange: '#f0ad4e',
+    green: '#5cb85c',
+};
+
+export const coursesColorSet = [
+    {
+        text: 'Red',
+        hex: '#FF3B30',
+    },
+    {
+        text: 'Orange',
+        hex: '#FF9500',
+    },
+    {
+        text: 'Green',
+        hex: '#34C759',
+    },
+    {
+        text: 'Blue',
+        hex: '#007AFF',
+    },
+    {
+        text: 'Purple',
+        hex: '#AF52DE',
+    },
+    {
+        text: 'Pink',
+        hex: '#F296D3',
+    },
+    {
+        text: 'Big Red',
+        hex: '#AB1D1A',
+    },
+    {
+        text: 'Chimes',
+        hex: '#EAB168',
+    },
+    {
+        text: 'Gorges',
+        hex: '#318663',
+    },
+    {
+        text: 'Cayuga',
+        hex: '#0D49A9',
+    },
+    {
+        text: 'Ithaca Winter',
+        hex: '#88B9F2',
+    },
+    {
+        text: 'Blossom',
+        hex: '#D67B9B',
+    },
+];

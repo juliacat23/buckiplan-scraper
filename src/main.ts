cf676503702508ef95f3f6e5a0e58541f811b61d
Julia Catalano <catalano.43@osu.edu>
2022-05-09 10:55:23 -0400

    config home page

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import './assets/scss/app.scss';
+import './assets/scss/introjs.scss';
 
 createApp(App).use(store).use(router).mount('#app');
